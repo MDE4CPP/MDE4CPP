fb9694a430a954044ffd4a993d6081f42c8134a0
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2017-07-28 16:10:14 +0200

    update generated files for release

---
+++ src_gen/fUML/FUMLPlugin.cpp
@@ -0,0 +1,23 @@
+
+#include "FUMLPlugin.hpp"
+#include "impl/FUMLPluginImpl.hpp"
+
+using namespace fUML;
+
+//static initialisation
+std::shared_ptr<MDE4CPPPlugin> FUMLPlugin::instance;
+
+std::shared_ptr<MDE4CPPPlugin> FUMLPlugin::eInstance()
+{
+	if(instance==nullptr)
+	{
+		//create a new Singelton Instance
+		instance.reset(new FUMLPluginImpl());
+	}
+	return instance;
+}
+
+std::shared_ptr<MDE4CPPPlugin> start()
+{
+	return FUMLPlugin::eInstance();
+}
