f896bbe32e61e73715e35b0c7613004d41e715a9
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-03-03 07:46:17 +0100

    clear includes in class LoadHandler

--- persistence/base/LoadHandler.cpp
+++ persistence/base/LoadHandler.cpp
@@ -8,46 +8,46 @@
 #include "persistence/base/LoadHandler.hpp"
 
 #include <sstream> // used for getLevel()
-#include <boost/algorithm/string.hpp> // used for string splitting
+#include "boost/algorithm/string.hpp" // used for string splitting
+#include "ecore/EObject.hpp"
+#include "ecore/EStructuralFeature.hpp"
+#include "persistence/base/HandlerHelper.hpp"
 
-namespace persistence
-{
-namespace base
-{
+using namespace persistence::base;
 
-LoadHandler::LoadHandler ()
+LoadHandler::LoadHandler()
 {
 	m_rootObject = nullptr;
 	m_level = -1;
 }
 
-LoadHandler::~LoadHandler ()
+LoadHandler::~LoadHandler()
 {
 
 }
 
-std::shared_ptr<ecore::EObject> LoadHandler::getObjectByRef ( std::string ref )
+std::shared_ptr<ecore::EObject> LoadHandler::getObjectByRef(std::string ref)
 {
 	std::shared_ptr<ecore::EObject> tmp;
 
-	if ( !ref.empty() )
+	if (!ref.empty())
 	{
-		if ( m_refToObject_map.find( ref ) != m_refToObject_map.end() )
+		if (m_refToObject_map.find(ref) != m_refToObject_map.end())
 		{
 			// found
-			tmp = m_refToObject_map.at( ref );
+			tmp = m_refToObject_map.at(ref);
 			//return std::dynamic_pointer_cast<ecore::EObject>( tmp );
 		}
 		else
 		{
-			size_t double_dot = ref.find( "#//", 0 );
-			std::string _ref_prefix = ref.substr( 0, double_dot ); // TODO '_ref_prefix' is not used in this case
-			std::string _ref_name = ref.substr( double_dot );
+			size_t double_dot = ref.find("#//", 0);
+			std::string _ref_prefix = ref.substr(0, double_dot); // TODO '_ref_prefix' is not used in this case
+			std::string _ref_name = ref.substr(double_dot);
 
-			if ( m_refToObject_map.find( _ref_name ) != m_refToObject_map.end() )
+			if (m_refToObject_map.find(_ref_name) != m_refToObject_map.end())
 			{
 				// found
-				tmp = m_refToObject_map.at( _ref_name );
+				tmp = m_refToObject_map.at(_ref_name);
 				//return std::dynamic_pointer_cast<ecore::EObject>( tmp );
 			}
 			else
@@ -60,16 +60,16 @@ std::shared_ptr<ecore::EObject> LoadHandler::getObjectByRef ( std::string ref )
 	return tmp;
 }
 
-void LoadHandler::addToMap ( std::shared_ptr<ecore::EObject> object )
+void LoadHandler::addToMap(std::shared_ptr<ecore::EObject> object)
 {
-	std::string ref = persistence::base::HandlerHelper::extractReference( object, m_rootObject, m_rootPrefix );
+	std::string ref = persistence::base::HandlerHelper::extractReference(object, m_rootObject, m_rootPrefix);
 
-	if ( !ref.empty() )
+	if (!ref.empty())
 	{
-		if ( m_refToObject_map.find( ref ) == m_refToObject_map.end() )
+		if (m_refToObject_map.find(ref) == m_refToObject_map.end())
 		{
 			// ref not found in map, so insert
-			m_refToObject_map.insert( std::pair<std::string, std::shared_ptr<ecore::EObject>>( ref, object ) );
+			m_refToObject_map.insert(std::pair<std::string, std::shared_ptr<ecore::EObject>>(ref, object));
 
 			MSG_DEBUG("Add to map: '" << ref << "'");
 		}
@@ -77,35 +77,35 @@ void LoadHandler::addToMap ( std::shared_ptr<ecore::EObject> object )
 }
 /**/
 
-std::string LoadHandler::getPrefix ()
+std::string LoadHandler::getPrefix()
 {
 	return m_rootPrefix;
 }
 
-std::string LoadHandler::getLevel ()
+std::string LoadHandler::getLevel()
 {
 	std::stringstream ss;
-	for ( int ii = 0; ii < m_level; ii++ )
+	for (int ii = 0; ii < m_level; ii++)
 	{
 		ss << " ";
 	}
 	return ss.str();
 }
 
-void LoadHandler::setCurrentObject ( std::shared_ptr<ecore::EObject> object )
+void LoadHandler::setCurrentObject(std::shared_ptr<ecore::EObject> object)
 {
-	if ( m_rootObject == nullptr )
+	if (m_rootObject == nullptr)
 	{
 		m_rootObject = object;
 	}
 	m_level++;
-	m_currentObjects.push_back( object );
+	m_currentObjects.push_back(object);
 }
 
-std::shared_ptr<ecore::EObject> LoadHandler::getCurrentObject ()
+std::shared_ptr<ecore::EObject> LoadHandler::getCurrentObject()
 {
 	std::shared_ptr<ecore::EObject> tmp_obj = m_currentObjects.back();
-	assert( tmp_obj );
+	assert(tmp_obj);
 
 	return tmp_obj;
 }
@@ -117,16 +117,16 @@ std::shared_ptr<ecore::EObject> LoadHandler::getCurrentObject ()
  * ::ecorecpp::mapping::type_traits::string_t serializer::get_type(EObject_ptr obj) const
  *
  */
-std::string LoadHandler::extractType ( std::shared_ptr<ecore::EObject> obj ) const
+std::string LoadHandler::extractType(std::shared_ptr<ecore::EObject> obj) const
 {
-	return persistence::base::HandlerHelper::extractType( obj, m_rootPrefix );
+	return persistence::base::HandlerHelper::extractType(obj, m_rootPrefix);
 }
 
-void LoadHandler::release ()
+void LoadHandler::release()
 {
 	std::shared_ptr<ecore::EObject> tmp_obj = m_currentObjects.back();
 
-	if ( tmp_obj == nullptr )
+	if (tmp_obj == nullptr)
 	{
 		MSG_ERROR("You can't call " << __PRETTY_FUNCTION__ << " while current Object is nullptr.");
 	}
@@ -138,13 +138,13 @@ void LoadHandler::release ()
 	}
 }
 
-void LoadHandler::addUnresolvedReference ( const std::string &name, std::shared_ptr<ecore::EObject> object, std::shared_ptr<ecore::EStructuralFeature> esf )
+void LoadHandler::addUnresolvedReference(const std::string &name, std::shared_ptr<ecore::EObject> object, std::shared_ptr<ecore::EStructuralFeature> esf)
 {
-	if ( object != nullptr )
+	if (object != nullptr)
 	{
-		if ( esf != nullptr )
+		if (esf != nullptr)
 		{
-			m_unresolvedReferences.push_back( persistence::base::UnresolvedReference( name, object, esf ) );
+			m_unresolvedReferences.push_back(persistence::base::UnresolvedReference(name, object, esf));
 		}
 		else
 		{
@@ -153,13 +153,13 @@ void LoadHandler::addUnresolvedReference ( const std::string &name, std::shared_
 	}
 	else
 	{
-		MSG_ERROR(MSG_FLF  << " object is a nullptr");
+		MSG_ERROR(MSG_FLF << " object is a nullptr");
 	}
 }
 
-void LoadHandler::resolveReferences ()
+void LoadHandler::resolveReferences()
 {
-	while ( !m_unresolvedReferences.empty() )
+	while (!m_unresolvedReferences.empty())
 	{
 		persistence::base::UnresolvedReference uref = m_unresolvedReferences.back();
 		m_unresolvedReferences.pop_back();
@@ -173,18 +173,18 @@ void LoadHandler::resolveReferences ()
 
 		try
 		{
-			if ( esf->getUpperBound() == 1 )
+			if (esf->getUpperBound() == 1)
 			{
 				// EStructuralFeature is a single object
 
-				resolved_object = this->getObjectByRef( name );
+				resolved_object = this->getObjectByRef(name);
 				//assert(resolved_object);
-				if ( resolved_object )
+				if (resolved_object)
 				{
-					references.push_back( resolved_object );
+					references.push_back(resolved_object);
 
 					// Call resolveReferences() of corresponding 'object'
-					object->resolveReferences( esf->getFeatureID(), references );
+					object->resolveReferences(esf->getFeatureID(), references);
 					//object->eSet(esf, resolved_object); // use eSet() instead resolveReferences()
 				}
 			}
@@ -195,31 +195,27 @@ void LoadHandler::resolveReferences ()
 				std::list<std::string> _strs;
 				std::string _tmpStr;
 
-				boost::split( _strs, name, boost::is_any_of( " " ) );
-				while ( _strs.size() > 0 )
+				boost::split(_strs, name, boost::is_any_of(" "));
+				while (_strs.size() > 0)
 				{
 					_tmpStr = _strs.front();
-					if ( std::string::npos != _tmpStr.find( "#//" ) )
+					if (std::string::npos != _tmpStr.find("#//"))
 					{
-						resolved_object = this->getObjectByRef( _tmpStr );
-						if ( resolved_object )
+						resolved_object = this->getObjectByRef(_tmpStr);
+						if (resolved_object)
 						{
-							references.push_back( resolved_object );
+							references.push_back(resolved_object);
 						}
 					}
 					_strs.pop_front();
 				}
 				// Call resolveReferences() of corresponding 'object'
-				object->resolveReferences( esf->getFeatureID(), references );
+				object->resolveReferences(esf->getFeatureID(), references);
 			}
 		}
-		catch ( std::exception& e )
+		catch (std::exception& e)
 		{
 			MSG_ERROR(MSG_FLF << " Exception: " << e.what());
 		}
 	}
 }
-
-} /* namespace base */
-} /* namespace persistence */
-
--- persistence/base/LoadHandler.hpp
+++ persistence/base/LoadHandler.hpp
@@ -5,84 +5,81 @@
  *      Author: Alexander
  */
 
-#ifndef LoadHandler_HPP_
-#define LoadHandler_HPP_
+#ifndef PERSISTENCE_BASE_LOADHANDLER_HPP
+#define PERSISTENCE_BASE_LOADHANDLER_HPP
 
 #include "persistence/interface/XLoadHandler.hpp"
-#include "persistence/base/HandlerHelper.hpp"
 
-#include <map>
 #include <list>
+#include <map>
+#include <memory>
 #include <string>
-#include <set>
-#include <exception>
 
-#include "boost/exception/to_string.hpp"
-
-#include "ecore/EObject.hpp"
-#include "ecore/EStructuralFeature.hpp"
+namespace ecore
+{
+	class EObject;
+	class EStructuralFeature;
+}
 
 namespace persistence
 {
-namespace base
-{
-struct UnresolvedReference
-{
-	inline UnresolvedReference ( std::string refName, std::shared_ptr<ecore::EObject> eObject, std::shared_ptr<ecore::EStructuralFeature> eStructuralFeature ) :
-					refName( refName ),
-					eObject( eObject ),
-					eStructuralFeature( eStructuralFeature )
+	namespace base
 	{
-	}
+		struct UnresolvedReference
+		{
+				inline UnresolvedReference(std::string refName, std::shared_ptr<ecore::EObject> eObject, std::shared_ptr<ecore::EStructuralFeature> eStructuralFeature) :
+					refName(refName), eObject(eObject), eStructuralFeature(eStructuralFeature)
+				{
+				}
 
-	std::string refName;
-	std::shared_ptr<ecore::EObject> eObject;
-	std::shared_ptr<ecore::EStructuralFeature> eStructuralFeature;
-};
+				std::string refName;
+				std::shared_ptr<ecore::EObject> eObject;
+				std::shared_ptr<ecore::EStructuralFeature> eStructuralFeature;
+		};
 
-/**
- * Class LoadHandler
- */
-class LoadHandler : public persistence::interface::XLoadHandler
-{
-public:
-	LoadHandler ();
-	virtual ~LoadHandler ();
+		/**
+		 * Class LoadHandler
+		 */
+		class LoadHandler: public persistence::interface::XLoadHandler
+		{
+			public:
+				LoadHandler();
+				virtual ~LoadHandler();
 
-	std::shared_ptr<ecore::EObject> getObjectByRef ( std::string ref );
+				std::shared_ptr<ecore::EObject> getObjectByRef(std::string ref);
 
-	std::string getLevel ();
-	std::string getPrefix ();
+				std::string getLevel();
+				std::string getPrefix();
 
-	std::string extractType ( std::shared_ptr<ecore::EObject> obj ) const;
+				std::string extractType(std::shared_ptr<ecore::EObject> obj) const;
 
-	void addToMap ( std::shared_ptr<ecore::EObject> object );
+				void addToMap(std::shared_ptr<ecore::EObject> object);
 
-	void setCurrentObject ( std::shared_ptr<ecore::EObject> object );
-	std::shared_ptr<ecore::EObject> getCurrentObject ();
+				void setCurrentObject(std::shared_ptr<ecore::EObject> object);
+				std::shared_ptr<ecore::EObject> getCurrentObject();
 
-	void release ();
+				void release();
 
-	virtual unsigned int getNumOfChildNodes () = 0;
-	virtual std::string getNextNodeName () = 0;
-	virtual std::map<std::string, std::string> getAttributeList () = 0;
+				virtual unsigned int getNumOfChildNodes() = 0;
+				virtual std::string getNextNodeName() = 0;
+				virtual std::map<std::string, std::string> getAttributeList() = 0;
 
-	void addUnresolvedReference ( const std::string &name, std::shared_ptr<ecore::EObject> object, std::shared_ptr<ecore::EStructuralFeature> esf );
+				void addUnresolvedReference(const std::string &name, std::shared_ptr<ecore::EObject> object, std::shared_ptr<ecore::EStructuralFeature> esf);
 
-	void resolveReferences ();
+				void resolveReferences();
 
-protected:
-	int m_level;
+			protected:
+				int m_level;
 
-	std::shared_ptr<ecore::EObject> m_rootObject;
-	std::list<std::shared_ptr<ecore::EObject> > m_currentObjects;
+				std::shared_ptr<ecore::EObject> m_rootObject;
+				std::list<std::shared_ptr<ecore::EObject> > m_currentObjects;
 
-	std::string m_rootPrefix;
+				std::string m_rootPrefix;
 
-	std::map<std::string, std::shared_ptr<ecore::EObject>> m_refToObject_map;
-	std::list<persistence::base::UnresolvedReference> m_unresolvedReferences;
-};
-} /* namespace base */
+				std::map<std::string, std::shared_ptr<ecore::EObject>> m_refToObject_map;
+				std::list<persistence::base::UnresolvedReference> m_unresolvedReferences;
+		};
+	} /* namespace base */
 } /* namespace persistence */
 
 #endif /* LoadHandler_HPP_ */
