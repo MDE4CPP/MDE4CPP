a8b7476ff2f51b9edb40839c223826144a02959b
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-03-03 12:26:43 +0100

    clear includes in class SaveHandler

--- persistence/base/SaveHandler.cpp
+++ persistence/base/SaveHandler.cpp
@@ -7,57 +7,54 @@
 
 #include "persistence/base/SaveHandler.hpp"
 
+#include <string>
 #include <sstream> // used for addReferences()
-#include <boost/algorithm/string.hpp> // used for string splitting
 
-namespace persistence
-{
-namespace base
-{
+#include "boost/algorithm/string.hpp" // used for string splitting
+#include "ecore/EObject.hpp"
+#include "persistence/base/HandlerHelper.hpp"
 
-SaveHandler::SaveHandler ()
+using namespace persistence::base;
+
+SaveHandler::SaveHandler()
 {
 	m_rootObject = nullptr;
 }
 
-SaveHandler::~SaveHandler ()
+SaveHandler::~SaveHandler()
 {
-
 }
 
 /**/
-std::string SaveHandler::getPrefix ()
+std::string SaveHandler::getPrefix()
 {
 	return m_rootPrefix;
 }
 
-void SaveHandler::setRootObject ( std::shared_ptr<ecore::EObject> object )
+void SaveHandler::setRootObject(std::shared_ptr<ecore::EObject> object)
 {
-	if ( m_rootObject == nullptr )
+	if (m_rootObject == nullptr)
 	{
 		m_rootObject = object;
 	}
 }
 
-void SaveHandler::addAttribute ( const std::string &name, bool value )
+void SaveHandler::addAttribute(const std::string &name, bool value)
 {
-	addAttribute( name, (value ? (std::string) "true" : (std::string) "false") );
+	addAttribute(name, (value ? (std::string) "true" : (std::string) "false"));
 }
 
-void SaveHandler::addReference ( const std::string &name, std::shared_ptr<ecore::EObject> object )
+void SaveHandler::addReference(const std::string &name, std::shared_ptr<ecore::EObject> object)
 {
-	if ( object )
+	if (object)
 	{
-		std::string ref = persistence::base::HandlerHelper::extractReference( object, m_rootObject, m_rootPrefix );
+		std::string ref = persistence::base::HandlerHelper::extractReference(object, m_rootObject, m_rootPrefix);
 
-		addAttribute( name, ref );
+		addAttribute(name, ref);
 	}
 }
 
-std::string SaveHandler::extractType ( std::shared_ptr<ecore::EObject> obj ) const
+std::string SaveHandler::extractType(std::shared_ptr<ecore::EObject> obj) const
 {
-	return persistence::base::HandlerHelper::extractType( obj, m_rootPrefix );
+	return persistence::base::HandlerHelper::extractType(obj, m_rootPrefix);
 }
-
-} /* namespace base */
-} /* namespace persistence */
--- persistence/base/SaveHandler.hpp
+++ persistence/base/SaveHandler.hpp
@@ -5,55 +5,51 @@
  *      Author: Alexander
  */
 
-#ifndef SaveHandler_HPP_
-#define SaveHandler_HPP_
+#ifndef PERSISTENCE_BASE_SAVEHANDLER_HPP
+#define PERSISTENCE_BASE_SAVEHANDLER_HPP
 
 #include "persistence/interface/XSaveHandler.hpp"
-#include "persistence/base/HandlerHelper.hpp"
 
-#include <map>
-#include <list>
 #include <string>
-#include <set>
-#include <exception>
 
-#include "boost/exception/to_string.hpp"
-
-#include "ecore/EObject.hpp"
+namespace ecore
+{
+	class EObject;
+}
 
 namespace persistence
 {
-namespace base
-{
-class SaveHandler : public persistence::interface::XSaveHandler
-{
-public:
-	SaveHandler ();
-	virtual ~SaveHandler ();
+	namespace base
+	{
+		class SaveHandler: public persistence::interface::XSaveHandler
+		{
+			public:
+				SaveHandler();
+				virtual ~SaveHandler();
 
-	std::string getPrefix ();
-	void setRootObject ( std::shared_ptr<ecore::EObject> object );
+				std::string getPrefix();
+				void setRootObject(std::shared_ptr<ecore::EObject> object);
 
-	std::string extractType ( std::shared_ptr<ecore::EObject> obj ) const;
+				std::string extractType(std::shared_ptr<ecore::EObject> obj) const;
 
-	virtual bool createRootNode ( const std::string& name, const std::string& ns_uri ) = 0;
-	virtual bool createRootNode ( const std::string& prefix, const std::string& name, const std::string& ns_uri ) = 0;
+				virtual bool createRootNode(const std::string& name, const std::string& ns_uri) = 0;
+				virtual bool createRootNode(const std::string& prefix, const std::string& name, const std::string& ns_uri) = 0;
 
-	virtual bool createAndAddElement ( const std::string& name ) = 0;
+				virtual bool createAndAddElement(const std::string& name) = 0;
 
-	void addAttribute ( const std::string &name, bool value );
-	virtual void addAttribute ( const std::string &name, const std::string& value ) = 0;
+				void addAttribute(const std::string &name, bool value);
+				virtual void addAttribute(const std::string &name, const std::string& value) = 0;
 
-	void addReference ( const std::string &name, std::shared_ptr<ecore::EObject> object );
-	virtual void addReferences ( const std::string &name, std::shared_ptr<ecore::EObject> object ) = 0;
+				void addReference(const std::string &name, std::shared_ptr<ecore::EObject> object);
+				virtual void addReferences(const std::string &name, std::shared_ptr<ecore::EObject> object) = 0;
 
-	virtual void release () = 0;
+				virtual void release() = 0;
 
-protected:
-	std::shared_ptr<ecore::EObject> m_rootObject;
-	std::string m_rootPrefix;
-};
-} /* namespace base */
+			protected:
+				std::shared_ptr<ecore::EObject> m_rootObject;
+				std::string m_rootPrefix;
+		};
+	} /* namespace base */
 } /* namespace persistence */
 
 #endif /* SaveHandler_HPP_ */
