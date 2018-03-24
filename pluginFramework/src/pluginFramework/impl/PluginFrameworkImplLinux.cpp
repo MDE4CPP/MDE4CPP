a264a9b6750fe0938c4680db2db3275f88c43fe9
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-03-24 11:11:35 +0100

    [pluginFramework] implement PluginFrameworkImplLinux::findAllAvailableLibraries

--- pluginFramework/src/pluginFramework/impl/PluginFrameworkImplLinux.cpp
+++ pluginFramework/src/pluginFramework/impl/PluginFrameworkImplLinux.cpp
@@ -13,11 +13,15 @@
   #define DEBUG_MESSAGE(a) a
 #endif
 
+#include <dirent.h>
 #include <iostream>
+#include <sstream>
+#include <unistd.h>
 
 #include "pluginFramework/MDE4CPPPlugin.hpp"
 
 #define MAX_CHAR 260
+using namespace std;
 
 PluginFrameworkImplLinux::PluginFrameworkImplLinux():
 	PluginFrameworkImpl()
@@ -37,15 +41,44 @@ PluginFramework* PluginFrameworkImplLinux::create()
 
 std::vector<std::string> PluginFrameworkImplLinux::findAllAvailableLibraries()
 {
-	char folderBuffer[MAX_CHAR];
-	char nameBuffer[MAX_CHAR];
+	char cCurrentPath[MAX_CHAR];
 	std::vector<std::string> libraries;
+	if (!getcwd(cCurrentPath, sizeof(cCurrentPath)))
+	{
+		return libraries;
+	}
 
+#ifdef NDEBUG
 	m_debugMode = false;
+#else
+	m_debugMode = true;
+#endif
 	m_endingDebug = "d.so";
 	m_endingRelease = ".so";
-	m_endingString = "d";
-	std::cerr << "PluginFrameworkImpl::findAllAvailableLibraries is not implemented for Linux systems!";
+
+	std::string folderName(cCurrentPath);
+	std::cout << folderName << std::endl;
+	DIR *dir;
+	struct dirent *file;
+	if((dir = opendir(folderName.c_str())) != NULL)
+	{
+		DEBUG_MESSAGE(std::cout << "libraries to be loaded" << std::endl;)
+		while((file = readdir(dir)) != NULL)
+		{
+			std::string libName = checkLibrary(file, folderName);
+			if (!libName.empty())
+			{
+				DEBUG_MESSAGE(std::cout << "\t" << libName << std::endl;)
+				libraries.push_back(libName);
+			}
+		}
+		closedir (dir);
+	}
+	else
+	{
+		std::cerr << "Could not open directory " << folderName << " failed" << std::endl;
+	}
+
 	return libraries;
 }
 
