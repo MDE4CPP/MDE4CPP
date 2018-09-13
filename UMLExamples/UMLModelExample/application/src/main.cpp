22fdf9f564340dc7f733ab28abba1223294f90f0
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-09-13 15:38:17 +0200

    [UMLModelExample] fix implementation of main function

--- UMLExamples/UMLModelExample/application/src/main.cpp
+++ UMLExamples/UMLModelExample/application/src/main.cpp
@@ -27,6 +27,21 @@ int main ()
 	std::shared_ptr<LibraryModel_umlFactory> factory = LibraryModel_umlFactory::eInstance();
 	std::shared_ptr<LibraryModel_umlPackage> package = LibraryModel_umlPackage::eInstance();
 
+	std::shared_ptr<Library> lib=factory->createLibrary();
+
+    std::shared_ptr<Book> book = factory->createBook_in_Library(lib);
+    book->setName("Magritte");
+
+    std::shared_ptr<Author> author = factory->createAuthor_in_Library(lib);
+    author->setName("David Sylvester");
+    book->getAuthors()->add(author);
+
+    std::shared_ptr<Picture> picture = factory->createPicture_in_Book(book);
+    picture->setName("Der Verrat der Binder (La trahison des images), 1929: Öl, 60x82 cm");
+    picture->setPageNumber(212);
+
+    lib->printLibrary();
+
 // Start of user code main
 // You may manually edit the following lines, won't be overwritten upon generation.
 
