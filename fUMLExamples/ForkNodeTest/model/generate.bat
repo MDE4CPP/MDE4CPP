cf143e8ef78a3de4bb335a386ff809f055fcdfae
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-09-13 18:51:18 +0200

    add batch files to generate source code of example models

---
+++ fUMLExamples/ForkNodeTest/model/generate.bat
@@ -0,0 +1,15 @@
+@echo off
+
+SET modelFolder=%cd%
+SET modelFile=%modelFolder%/forkNodeTest.uml
+
+:: Go to folder, in which the gradle task 'generateModel' is callable, e.g. the MDE4CPP home folder
+:: 'generateModel' is defined in %MDE4CPP_HOME%/src/common/universalGradleTasks/build.gradle
+CD /D %MDE4CPP_HOME%
+
+:: Call the gradle task with model path as parameter
+:: 'FUML4CPP' will be used. To use 'UML4CPP' add parameter -PSO
+CALL gradle generateModel -PModel=%modelFile%
+
+:: Go back to initial folder
+CD /D %modelFolder%
