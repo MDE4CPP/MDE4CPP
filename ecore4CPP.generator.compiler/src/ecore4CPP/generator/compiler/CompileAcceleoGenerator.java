8bc4ce3b7a81f9cc91be43621417b0caddca61f3
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-09-15 21:45:18 +0200

    support of java 10

--- ecore4CPP.generator.compiler/src/ecore4CPP/generator/compiler/CompileAcceleoGenerator.java
+++ ecore4CPP.generator.compiler/src/ecore4CPP/generator/compiler/CompileAcceleoGenerator.java
@@ -161,7 +161,7 @@ public class CompileAcceleoGenerator
 		}
 	}
 	
-	 public static void main(String[] args) {
+	 public static void main(final String[] args) {
         try 
         {
             if (args.length != 2) 
