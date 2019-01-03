9a7bb82ebde620b2da504c5a8541d29fe44684cf
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2019-01-03 09:40:28 +0100

    [UML4CPP] add documentation for validation service class and module, remove package as parameter from service functions

--- generator/UML4CPP/UML4CPP.generator/src/UML4CPP/generator/main/validation/ValidationService.java
+++ generator/UML4CPP/UML4CPP.generator/src/UML4CPP/generator/main/validation/ValidationService.java
@@ -5,40 +5,72 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.emf.common.EMFPlugin;
-import org.eclipse.uml2.uml.Package;
 
+/**
+ * Service class to report model errors found during C++ source code generation of given UML model<br>
+ * Provided functionality:<br>
+ * <ul>
+ * 	<li>register errors</li>
+ *  <li>print errors</li>
+ *  <li>check if errors registered</li>
+ *  <li>exit java program with error code, if at least one error is registered and generator is running outside of eclipse</li>
+ * </ul>
+ * Functionality must be triggered by generator modules.<br> In particular, error printing and exiting with error is not done automatically to wait for complete generator executing and collecting all existing errors.
+ * <br><br> 
+ * 
+ * @author wichmann
+ *
+ */
 public class ValidationService {
 
 	private List<String> errors;
 	
+	/**
+	 * Constructor<br>
+	 * initialize error list as ArrayList of type String
+	 */
 	public ValidationService() 
 	{
 		errors = new ArrayList<String>();
 	}
 	
+	/**
+	 * add {@code message} to internal error list
+	 * @param message String representing error message
+	 */
 	public void registerError(String message)
 	{
 		errors.add(message);
 	}
 	
-	public boolean errorExisting(Package pac)
+	/**
+	 * checks, if at least one error is registered
+	 * @return true for error existing, otherwise false
+	 */
+	public boolean errorExisting()
 	{
 		return errors.size() > 0;
 	}
 	
-	public void printErrors(Package pac)
+	/**
+	 * prints all registered errors to syserr
+	 */
+	public void printErrors()
 	{
-		if (errorExisting(pac))
+		if (errorExisting())
 		{
-			System.err.println("Validation of package " + pac.getName() + " failed with following errors:");
+			System.err.println("Generation failed with following errors:");
 			for (String errorMessage : errors) {
 				System.err.println("- " + errorMessage);
 			}
-			System.err.println("Generation failed");
 		}
 	}
 	
-	public void closeWithErrorExit(Package pac, int number)
+	/**
+	 * if error is registered and generator is running outside of eclipse, application will be closed with given number
+	 * @param number - exit code to be used during application exit, {@value 0} - close without error notification, otherwise close with error notification 
+	 */
+	public void closeWithErrorExit(int number)
 	{
 		if (!EMFPlugin.IS_ECLIPSE_RUNNING)
 		{
