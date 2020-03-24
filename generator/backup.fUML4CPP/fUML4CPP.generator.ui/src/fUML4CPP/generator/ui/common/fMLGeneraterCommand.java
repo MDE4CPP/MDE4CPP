506750cba10ecef77f1bdfea47cf2fd3a92b3d7d
Maximilian Hammer <maximilian.hammer@outlook.de>
2020-03-24 13:58:05 +0100

    [fUML4CPP] backup before PSCS adaptions

---
+++ generator/backup.fUML4CPP/fUML4CPP.generator.ui/src/fUML4CPP/generator/ui/common/fMLGeneraterCommand.java
@@ -0,0 +1,86 @@
+package fUML4CPP.generator.ui.common;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+import fUML4CPP.generator.ui.Activator;
+
+/**
+ * 
+ *
+ */
+public class fMLGeneraterCommand extends AbstractHandler
+{
+	@SuppressWarnings("unchecked")
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+	    if (window != null)
+	    {
+	        IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
+	        List<IFile> files = selection.toList();
+	        IRunnableWithProgress operation = new IRunnableWithProgress() {
+				public void run(IProgressMonitor monitor) {
+					try {
+						Iterator<IFile> filesIt = files.iterator();
+						while (filesIt.hasNext()) {
+							IFile model = (IFile)filesIt.next();
+							URI modelURI = URI.createPlatformResourceURI(model.getFullPath().toString(), true);
+							try {
+								File target = model.getRawLocation().toFile();
+								target = target.getParentFile();
+								if (target.getParentFile().exists())
+								{
+									target = target.getParentFile();
+								}
+					    		GenerateAll generator = new GenerateAll(modelURI, new File(target.getAbsoluteFile() + File.separator + "src_gen"), new ArrayList<String>());
+								generator.doGenerate(monitor);
+							} catch (IOException e) {
+								IStatus status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e);
+								Activator.getDefault().getLog().log(status);
+							} finally {
+								model.getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
+							}
+						}
+					} catch (CoreException e) {
+						IStatus status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e);
+						Activator.getDefault().getLog().log(status);
+					}
+				}
+			};
+			try {
+				PlatformUI.getWorkbench().getProgressService().run(true, true, operation);
+			} catch (InvocationTargetException e) {
+				IStatus status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e);
+				Activator.getDefault().getLog().log(status);
+			} catch (InterruptedException e) {
+				IStatus status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e);
+				Activator.getDefault().getLog().log(status);
+			}
+	    }
+
+		
+		return null;
+	}
+
+}
