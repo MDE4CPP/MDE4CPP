654f4c4e0a5a4f9be6c408a0ab369fe05ac2a46f
HammerMaximilian <58948559+HammerMaximilian@users.noreply.github.com>
2021-01-05 15:51:49 +0100

    [types] updated new source code

---
+++ src/ecore/types/src_gen/types/impl/typesFactoryImpl.cpp
@@ -0,0 +1,65 @@
+#include "types/impl/typesFactoryImpl.hpp"
+
+#include <cassert>
+
+#include "abstractDataTypes/SubsetUnion.hpp"
+#include "ecore/EClass.hpp"
+
+
+
+using namespace types;
+
+//*********************************
+// Constructor / Destructor
+//*********************************
+
+typesFactoryImpl::typesFactoryImpl()
+{
+}
+
+typesFactoryImpl::~typesFactoryImpl()
+{
+}
+
+typesFactory* typesFactoryImpl::create()
+{
+	return new typesFactoryImpl();
+}
+
+//*********************************
+// creators
+//*********************************
+
+std::shared_ptr<ecore::EObject> typesFactoryImpl::create(const int metaElementID, std::shared_ptr<ecore::EObject> container /*= nullptr*/, const int referenceID/* = -1*/) const
+{
+	switch(metaElementID)
+	{
+	default:
+	   	    std::cerr << __PRETTY_FUNCTION__ << " ID " << metaElementID <<" not found" << std::endl;
+	}
+	return nullptr;
+}
+
+std::shared_ptr<ecore::EObject> typesFactoryImpl::create(std::shared_ptr<ecore::EClass> _class, std::shared_ptr<ecore::EObject> container /*= nullptr*/, const int referenceID/* = -1*/) const
+{
+	if(_class->isAbstract())
+    {
+    	return nullptr;
+   	}
+	int _elementID = _class->getMetaElementID();
+	return create(_elementID, container, referenceID);
+}
+
+std::shared_ptr<ecore::EObject> typesFactoryImpl::create(std::string _className, std::shared_ptr<ecore::EObject> container /*= nullptr*/, const int referenceID/* = -1*/) const
+{
+	auto iter = m_idMap.find(_className);
+	if(iter != m_idMap.end())
+    {
+		//get the ID
+        int id = iter->second;
+		return create(id,container,referenceID);
+    }
+    return nullptr;
+}
+
+
