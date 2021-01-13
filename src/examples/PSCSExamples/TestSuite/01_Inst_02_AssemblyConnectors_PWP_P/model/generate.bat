a2cdae1ddc212ac2557af895bb8eff69089d567a
HammerMaximilian <58948559+HammerMaximilian@users.noreply.github.com>
2021-01-13 14:03:55 +0100

    [examples] added 'generate' and 'generate.bat' files to all PSCS test models

---
+++ src/examples/PSCSExamples/TestSuite/01_Inst_02_AssemblyConnectors_PWP_P/model/generate.bat
@@ -0,0 +1,15 @@
+@echo off
+
+SET modelFolder=%cd%
+SET modelFile=%modelFolder%/AssemblyConnectors_PWP_P.uml
+
+REM Go to MDE4CPP home folder to call the gradle task 'generateModel'
+REM Task 'generateModel' is defined in $MDE4CPP_HOME/src/common/universalGradleTasks/build.gradle
+CD /D %MDE4CPP_HOME%
+
+REM Call the task 'generateModel' with model path as parameter
+REM generator will be detected automatically (fUML4CPP is used here)
+CALL gradlew generateModel -PModel=%modelFile%
+
+:: Go back to initial folder
+CD /D %modelFolder%
