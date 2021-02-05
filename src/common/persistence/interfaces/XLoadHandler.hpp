3ee3c3648282777e69207d7e1085cbd221f97799
maschotta <ralph.maschotta@tu-ilmenau.de>
2021-02-05 15:10:20 +0100

    [persistence] Bugfix and additional functions to read template-container (map, list,vector...)

--- src/common/persistence/interfaces/XLoadHandler.hpp
+++ src/common/persistence/interfaces/XLoadHandler.hpp
@@ -9,8 +9,11 @@
 #define PERSISTENCE_INTERFACES_XLOADHANDLER_HPP
 
 #include <map>
+#include <list>
+#include <vector>
 #include <memory>
 #include <string>
+#include "abstractDataTypes/Bag.hpp"
 
 namespace ecore
 {
@@ -33,6 +36,36 @@ namespace persistence
 				virtual std::string getLevel() = 0;
 				virtual void addToMap(std::shared_ptr<ecore::EObject> object) = 0;
 				virtual void handleChild(std::shared_ptr<ecore::EObject> object) = 0;
+
+				template<typename T> void handleChildContainer(std::shared_ptr<Bag<T>> objects)
+				{
+					for (auto _object : *objects)
+					{
+						handleChild(_object);
+					}
+				}
+				template<typename T> void handleChildContainer(std::shared_ptr<std::list<T> > objects)
+				{
+					for (auto _object : *objects)
+					{
+						handleChild(_object);
+					}
+				}
+				template<typename T> void handleChildContainer(std::shared_ptr<std::vector<T> > objects)
+				{
+					for (auto _object : *objects)
+					{
+						handleChild(_object);
+					}
+				}
+				template<typename K, typename V> void handleChildContainer(std::shared_ptr<std::map<K,V> > objects)
+				{
+					for (auto _object : *objects)
+					{
+						handleChild(_object.second);
+					}
+				}
+
 				virtual std::shared_ptr<ecore::EObject> getCurrentObject() = 0;
 
 				virtual void release() = 0;
