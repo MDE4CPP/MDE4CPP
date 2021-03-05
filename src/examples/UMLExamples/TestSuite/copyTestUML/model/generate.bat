f990edca24885cb37cb686f6e4fcd6cf710c0bf0
maschotta <ralph.maschotta@tu-ilmenau.de>
2021-03-05 13:40:07 +0100

    [examples] add generate script to copyTest example

---
+++ src/examples/UMLExamples/TestSuite/copyTestUML/model/generate.bat
@@ -0,0 +1,15 @@
+@echo off
+
+SET modelFolder=%cd%
+SET modelFile=%modelFolder%/copyTestUML.uml
+
+REM Go to MDE4CPP home folder to call the gradle task 'generateModel'
+REM Task 'generateModel' is defined in $MDE4CPP_HOME/src/common/universalGradleTasks/build.gradle
+CD /D %MDE4CPP_HOME%
+
+REM Call the task 'generateModel' with model path as parameter
+REM generator will be detected automatically (UML4CPP is used because parameter SO (struktureOnly) is defined)
+CALL gradlew generateModel -PModel=%modelFile% -PSO
+
+REM Go back to model folder
+CD /D %modelFolder%
