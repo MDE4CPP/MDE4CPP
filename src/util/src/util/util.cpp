b42bbcbdc43028b2875c41a6a7174a6109b6634d
HammerMaximilian <58948559+HammerMaximilian@users.noreply.github.com>
2021-07-19 13:57:41 +0200

    [util] implemented polynomial rolling hash for generating IDs

--- src/util/src/util/util.cpp
+++ src/util/src/util/util.cpp
@@ -5,3 +5,22 @@ using namespace util;
 Util::Util()
 {
 }
+
+long long Util::polynomialRollingHash(std::string qualifiedName)
+{
+		//generate a hash value from string using polynomial rolling hash
+		int n = qualifiedName.size();
+		int m = (int) (10e9 + 9);
+		int p = 53;
+		long long powerOfP = 1;
+		long long hash = 0;
+		
+		for(int i = 0; i < n; i++)
+		{
+			int c = qualifiedName.at(i);
+			hash = (hash + c * powerOfP) % m;
+			powerOfP = (powerOfP * p) % m;
+		}
+		
+		return hash;
+}
--- src/util/src/util/util.hpp
+++ src/util/src/util/util.hpp
@@ -1,12 +1,17 @@
 #ifndef UTIL_UTIL_HPP
 #define UTIL_UTIL_HPP
 
+#include <string>
+
 namespace util
 {
 	class Util
 	{
 		public:
 			Util();
+			
+			static long long polynomialRollingHash(std::string);
+				
 	};
 }
 #endif
