b0bceb1c139023d73778e11f3b7f78c982acb3dc
maschotta <ralph.maschotta@tu-ilmenau.de>
2022-02-04 15:35:34 +0100

    [ecore4cpp] use unsigned Long instead of long long as IDs (alternative hash values for operations: less amount of hash values)

--- generator/ecore4CPP/ecore4CPP.generator/src/ecore4CPP/generator/main/helpers/IDGeneratorService.java
+++ generator/ecore4CPP/ecore4CPP.generator/src/ecore4CPP/generator/main/helpers/IDGeneratorService.java
@@ -6,7 +6,7 @@ public class IDGeneratorService {
 	{
 		//generate a hash value from string using polynomial rolling hash
 		int n = key.length();
-		int m = (int) (10e9 + 9);
+		int m = 2147483647; // 32 Bit Prime number
 		int p = 53;
 		long powerOfP = 1;
 		long hash = 0;
@@ -20,4 +20,25 @@ public class IDGeneratorService {
 		
 		return Long.toString(hash);
 	}
+
+	public String polynomialRollingHashShort(String key) 
+	{
+		//generate a hash value from string using polynomial rolling hash
+		int n = key.length();
+		//int m = 65537; // or 32 bit signed 32749 Prime Numbers; 
+		int m = (int) (10e9 + 9);
+		int p = 53;
+		int powerOfP = 1;
+		int hash = 0;
+		
+		for(int i = 0; i < n; i++)
+		{
+			int c = (short) key.charAt(i);
+			hash = (hash + c * powerOfP) % m;
+			powerOfP = (powerOfP * p) % m;
+		}
+		
+		return Integer.toUnsignedString((int)hash);
+	}
+
 }
