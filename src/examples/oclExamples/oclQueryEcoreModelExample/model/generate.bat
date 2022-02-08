041965f4a973c8c54bc3aa19c3f6bc6e308a1d5a
maschotta <ralph.maschotta@tu-ilmenau.de>
2022-02-08 10:19:53 +0100

    [ocl4cpp] add an example to query a model using OCL constatins

---
+++ src/examples/oclExamples/oclQueryEcoreModelExample/model/generate.bat
@@ -0,0 +1,15 @@
+@echo off
+
+SET modelFolder=%cd%
+SET modelFile=%modelFolder%/Library_QueryEcore.ecore
+
+REM Go to MDE4CPP home folder to call the gradle task 'generateModel'
+REM Task 'generateModel' is defined in $MDE4CPP_HOME/src/common/universalGradleTasks/build.gradle
+CD /D %MDE4CPP_HOME%
+
+REM Call the task 'generateModel' with model path as parameter
+REM generator will be detected automatically (ecore4CPP is used here)
+CALL gradlew generateModel -PModel=%modelFile%
+
+REM Go back to model folder
+CD /D %modelFolder%
