9a370f549c075950d83dc920af027ad35877eef2
Maximilian Hammer <maximilian.hammer@outlook.de>
2024-04-03 16:13:29 +0200

    [fUML] Erased include annotation from StructuralFeatureActionActivation

--- src/fuml/src_gen/fUML/Semantics/Actions/impl/StructuralFeatureActionActivationImpl.cpp
+++ src/fuml/src_gen/fUML/Semantics/Actions/impl/StructuralFeatureActionActivationImpl.cpp
@@ -35,8 +35,6 @@
 #include "ecore/EStructuralFeature.hpp"
 #include "ecore/ecorePackage.hpp"
 #include "ecore/ecoreFactory.hpp"
-//Includes from codegen annotation
-#include "fUML/MDE4CPP_Extensions/FUML_LinkEnd.hpp"
 //Forward declaration includes
 #include "persistence/interfaces/XLoadHandler.hpp" // used for Persistence
 #include "persistence/interfaces/XSaveHandler.hpp" // used for Persistence
