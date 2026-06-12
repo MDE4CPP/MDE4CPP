bc35a2953a691c571b82bed7ed20a20cf1e20d29
burhankhanzada <burhankhanzada199888@gmail.com>
2026-06-12 12:34:33 +0200

    fix: correct case sensitivity for bootstrap directory paths on windows

--- bootstrap.bat
+++ bootstrap.bat
@@ -6,7 +6,7 @@ REM # Windows Bootstrap script for MDE4CPP #
 REM ########################################
 
 set "DIR=%~dp0"
-call "%DIR%bootstrap\Windows\common.bat"
+call "%DIR%bootstrap\windows\common.bat"
 
 set "PROJECT_DIR=%~dp0"
 REM Remove trailing slash for PROJECT_DIR
@@ -16,39 +16,39 @@ for %%I in ("%PROJECT_DIR%\..") do set "PARENT_DIR=%%~fI"
 set "ECLIPSE_DIR=%PARENT_DIR%\eclipse"
 set "SETENV_FILE=%PROJECT_DIR%\setenv.bat"
 
-call "%DIR%bootstrap\Windows\common.bat" print_header "bootstrap" "MDE4CPP Bootstrap Script for Windows"
+call "%DIR%bootstrap\windows\common.bat" print_header "bootstrap" "MDE4CPP Bootstrap Script for Windows"
 
-call "%DIR%bootstrap\Windows\common.bat" print_header "bootstrap" "Installing system tools..."
+call "%DIR%bootstrap\windows\common.bat" print_header "bootstrap" "Installing system tools..."
 
-call "%DIR%bootstrap\Windows\installJava.bat"
+call "%DIR%bootstrap\windows\installJava.bat"
 if errorlevel 1 (
     echo %C_ERROR%ERROR: installJava.bat failed ^(exit !errorlevel!^)%C_RESET%
     exit /b !errorlevel!
 )
 
-call "%DIR%bootstrap\Windows\installCompiler.bat"
+call "%DIR%bootstrap\windows\installCompiler.bat"
 if errorlevel 1 (
     echo %C_ERROR%ERROR: installCompiler.bat failed ^(exit !errorlevel!^)%C_RESET%
     exit /b !errorlevel!
 )
 
-call "%DIR%bootstrap\Windows\installCMake.bat"
+call "%DIR%bootstrap\windows\installCMake.bat"
 if errorlevel 1 (
     echo %C_ERROR%ERROR: installCMake.bat failed ^(exit !errorlevel!^)%C_RESET%
     exit /b !errorlevel!
 )
 
-call "%DIR%bootstrap\Windows\installEclipse.bat"
+call "%DIR%bootstrap\windows\installEclipse.bat"
 if errorlevel 1 (
     echo %C_ERROR%ERROR: installEclipse.bat failed ^(exit !errorlevel!^)%C_RESET%
     exit /b !errorlevel!
 )
 
 
-call "%DIR%bootstrap\Windows\generate_setenv.bat"
+call "%DIR%bootstrap\windows\generate_setenv.bat"
 if errorlevel 1 exit /b 1
 
-call "%DIR%bootstrap\Windows\run_gradle_install.bat"
+call "%DIR%bootstrap\windows\run_gradle_install.bat"
 if errorlevel 1 exit /b 1
 
 echo %C_WARN%===========================================================%C_RESET%
